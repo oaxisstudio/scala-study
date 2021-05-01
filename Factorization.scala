388b15fb644a557908a902b0a7082fa82a276628
kaoru-t <oaxisstudio@gmail.com>
2021-05-01 23:19:44 +0900

    Scala基礎コース03 素因数分解 練習問題初級 - 関数の引数でtargetを渡す

--- Factorization.scala
+++ Factorization.scala
@@ -1,27 +1,25 @@
 import scala.math.sqrt
 
 object Factorization extends App {
-  var target = 90
-  // 割る数の最大値
-  var maxDivisor = sqrt(target).toInt
 
-  // 対象
-  var num = 24;
-  // 割る数
-  var divisor = 0;
-  // 結果
-  var acc = 0;
-  def factorizationRec(num: Int, divisor: Int, acc: Map[Int, Int]): Map[Int, Int] = {
-    if(divisor > maxDivisor) {
-      if (num == 1) acc else acc + (num -> 1)
-    } else if (num % divisor == 0) {
-      val nextAcc = acc + (divisor -> (acc.getOrElse(divisor, 0) + 1))
-      factorizationRec(num/divisor, divisor, nextAcc)
-    } else {
-      factorizationRec(num, divisor + 1, acc)
+  def factorization(target: Int):Map[Int, Int] = {
+    // 割る数の最大値
+    var maxDivisor = sqrt(target).toInt
+
+    def factorizationRec(num: Int, divisor: Int, acc: Map[Int, Int]): Map[Int, Int] = {
+      if(divisor > maxDivisor) {
+        if (num == 1) acc else acc + (num -> 1)
+      } else if (num % divisor == 0) {
+        val nextAcc = acc + (divisor -> (acc.getOrElse(divisor, 0) + 1))
+        factorizationRec(num/divisor, divisor, nextAcc)
+      } else {
+        factorizationRec(num, divisor + 1, acc)
+      }
     }
+
+    factorizationRec(target, 2, Map())
   }
 
-  println(factorizationRec(target, 2, Map()))
+  println(factorization(510510))
 }
 
